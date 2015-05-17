51143159c18162012e66ac93062852adadb4d839
OmniCypher <OmniCypher@yahoo.com>
2015-05-17 16:30:40 -0700

    Kinda fixed Money v0.1

--- Networking Test/Assets/standard JS/cash.js
+++ Networking Test/Assets/standard JS/cash.js
@@ -4,30 +4,32 @@ import UnityEngine;
 
 var cashFlow:int;
 public var cashReady:int;
-var time:float;
+var time : float;
 public var displayCash;
 public var started:boolean;
+public var period : float = 5.0;
 // Use this for initialization
 function Start () {
 	cashReady = 100;
 	cashFlow = 10;
 	started = false;
-	
+	displayCash = "0";
 }
 
 // Update is called once per frame
 function Update () {
-	
-if(Network.isServer || Network.isClient)
-{
-	if(cashReady != int.Parse(displayCash.text))
-		cashReady = displayCash.text;
-	if (time <= Time.time - 3) {
-		time=Time.time;
-		cashReady+=cashFlow;
+	if((Network.isServer || Network.isClient) && started){
+		Invoke("addCash",period);
+		if(cashReady != int.Parse(displayCash.text))
+			cashReady = int.Parse(displayCash.text);
+		updateDisplay();
 	}
-	displayCash = ("$" + cashReady.ToString());
 }
+
+public function addCash(){
+	CancelInvoke();
+	cashReady+=cashFlow;
+	updateDisplay();
 }
 
 public function changeCash(temp:int){
@@ -37,12 +39,23 @@ public function changeCash(temp:int){
 public function changeFlow(temp:int){
 	cashFlow+=temp;
 }
+
 public function getCash(){
 	return cashReady;
 }
+
+public function updateDisplay(){
+	GameObject.FindWithTag("money").GetComponent(UI.Text).text = (cashReady.ToString());
+}
+
 function OnConnectedToServer(){
+	print("OnConnectedToServer");
+	started = true;
 	displayCash = GameObject.FindWithTag("money").GetComponent(UI.Text);
 }
+
 function OnPlayerConnection() {
+	print("OnPlayerConnection");
+	started = true;
 	displayCash = GameObject.FindWithTag("money").GetComponent(UI.Text);
 }
