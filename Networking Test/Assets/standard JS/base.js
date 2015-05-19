fc6688a4ede061d12d54e72901bc84f0f35a4d6e
matthewgillogly <matthewgillogly@gmail.com>
2015-05-18 20:48:14 -0700

    Base Health added

--- Networking Test/Assets/standard JS/base.js
+++ Networking Test/Assets/standard JS/base.js
@@ -1,10 +1,29 @@
 ﻿#pragma strict
+import UnityEngine.UI;
+
 var health:int;
+var yourHealth:UI.Text;
+var enemyHealth:UI.Text;
 function Start () {
 health =100;
+
 }
 
 function Update () {
+	if(Network.isServer)
+	{
+		if(transform.tag == "Hero Base" && int.Parse(yourHealth.text.Substring(0,yourHealth.text.Length-1)) != health)
+			yourHealth.text = (health + "%");
+		else if(transform.tag == "Zero Base" && int.Parse(enemyHealth.text.Substring(0,enemyHealth.text.Length-1)) != health)
+			enemyHealth.text = (health + "%");
+	}
+	else if(Network.isClient)
+	{
+		if(this.gameObject.tag == "Zero Base" && int.Parse(yourHealth.text.Substring(0,yourHealth.text.Length-1)) != health)
+			yourHealth.text = (health + "%");
+		else if(this.gameObject.tag == "Hero Base" && int.Parse(enemyHealth.text.Substring(0,enemyHealth.text.Length-1)) != health)
+			enemyHealth.text = (health + "%");
+	}
 	if(health<=0){
 		print(transform.tag+" dead");
 		Application.Quit();
